83cef96800c18f614af0b6a11848a6f56ed2c696
AnselmSchaefer <anselmschaefer91@gmail.com>
2025-08-06 15:23:11 +0200

    initial

---
+++ src/main/java/com/barsch/petison/PetisonApplication.java
@@ -0,0 +1,13 @@
+package com.barsch.petison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetisonApplication.class, args);
+	}
+
+}
