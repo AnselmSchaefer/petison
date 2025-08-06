943c666b406b00e4a03d1ff9e242277883a0d439
AnselmSchaefer <anselmschaefer91@gmail.com>
2025-08-06 16:18:53 +0200

    simple endpoint

--- src/main/java/com/barsch/petison/PetisonApplication.java
+++ src/main/java/com/barsch/petison/PetisonApplication.java
@@ -2,9 +2,21 @@ package com.barsch.petison;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class PetisonApplication {
+
+	@GetMapping("/api")
+	public String sampleEndpoint() {
+		return """
+        	Get busy living
+        	or
+        	get busy dying.
+        	--Stephen King""";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetisonApplication.class, args);
